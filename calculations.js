9eb4007e8c0ec737a017b8dc1468a3f578b0bed1
Lota Plezere <lota.plezere@gmail.com>
2016-12-30 18:10:23 +0200

    Functional site, but note edits still needed

--- calculations.js
+++ calculations.js
@@ -1,3 +1,4 @@
+// ------- DISPLAY TODAY'S DATE ----------
 
 function getToday() {
   var todaysFullDate = new Date();
@@ -18,32 +19,232 @@ function displayDate() {
 displayDate();
 
 
+// ------- DISPLAY AVAILABLE CURRENCIES IN THE DROPDOWN, LOAD RATES JSON----------
 
+var rates;
 
+function displayAvailableCurrencies(){
 
+  var currencyUrl = "http://api.fixer.io/latest";
 
-
-
-
-
-
-function getRates() {
-  var baseCurrency = "EUR";
-  var targetCurrency = "USD";
-  var targetCalc = "http://api.fixer.io/latest?symbols=" + targetCurrency;
-  $.getJSON(targetCalc, function(data){
-    console.log(data.rates);
-
-    var valueDisplay = document.getElementById("valueDisplay");
-    valueDisplay.innerHTML = "Exchange rate from " + baseCurrency + " to " + targetCurrency + " is " + data.rates.USD;
-
-
-    // JSON.stringify(data.rates);
-    // valueDisplay.innerHTML = JSON.stringify(JSON.parse(data.rates));
+  $.getJSON(currencyUrl, function(data){
+    $.each(data.rates, function(i) {
+      var option = "<option>"+i+"</option>";
+      $("#fromCurrency").append($(option));
+      $("#toCurrency").append($(option));
+      rates = data.rates;
+    });
   });
 }
 
-getRates();
+displayAvailableCurrencies();
+
+
+// ---------------- SHOW THE FINAL EXCHANGE RESULT --------------------
+
+
+function getRates() {
+  var baseCurrency = $("#fromCurrency option:selected").text();
+  var targetCurrency = $( "#toCurrency option:selected" ).text();
+  // var targetCalc = "http://api.fixer.io/latest?symbols=" + targetCurrency;
+
+  if (baseCurrency === "EUR") {
+    console.log(baseCurrency + " base (FROM), target is " + targetCurrency );
+    var result = rates[targetCurrency];
+    console.log(result);
+    valueDisplay.innerHTML = "rate from " + baseCurrency + " to " + targetCurrency + " is: " + result;
+
+} else {
+
+  console.log(baseCurrency + " base (FROM), target is " + targetCurrency );
+
+  var baseChangerUrl = "http://api.fixer.io/latest?base=" + baseCurrency;
+  console.log(baseChangerUrl);
+  $.getJSON(baseChangerUrl, function(data){
+
+      var newBaseRates = data.rates;
+
+      console.log("base changed to " + baseCurrency);
+      console.log(data.rates);
+
+      var newBaseResult = newBaseRates[targetCurrency];
+      console.log(newBaseResult);
+
+      valueDisplay.innerHTML = "rate from " + baseCurrency + " to " + targetCurrency + " is: " + newBaseResult;
+
+
+
+// COMPARISON TO YESTERDAY
+
+      // console.log(getToday());
+      // console.log(getToday().day);
+      // var yesterdayDay = (getToday().day) - 1;
+      // console.log(yesterdayDay);
+      // var currentMonth = (getToday().month);
+      // console.log(currentMonth);
+      //
+      //
+      // var currentYear = new Date().getFullYear();
+      // console.log(currentYear);
+      // var yesterdaysDate = currentYear + "-" + currentMonth + "-" + yesterdayDay;
+      // console.log(yesterdaysDate);
+      //
+      // var yesterdaysDateRateUrl =  "http://api.fixer.io/" + yesterdaysDate;
+      // console.log(yesterdaysDateRateUrl);
+      //
+      // $.getJSON(yesterdaysDateRateUrl, function(data){
+      //     var yesterdayRates = data.rates;
+      //     console.log(yesterdayRates);
+      //     var yesterdayResult = yesterdayRates[targetCurrency];
+      //     console.log("yesterday the rate was" + yesterdayResult);
+      //
+      //     })
+
+
+
+
+      // $.getJSON("http://api.fixer.io/2000-01-03", function(data){
+      //
+      // }
+      //
+      // historyDisplay.innerHTML = "Yesterday this currency  rate was" + baseCurrency + " to " + targetCurrency + " is: " + newBaseResult;
+
+      // var dailyDisplay = document.getElementById("dailyDisplay");
+      // dailyDisplay.innerHTML = todayDate.day + "." + todayDate.month;
+
+      // var yesterday =
+      //
+      // historyDisplay.innerHTML = ""
+    });
+
+  }
+}
+
+
+  //
+  // var selectedTargetRate = rates[targetCurrency];
+  // console.log(selectedTargetRate);
+  // valueDisplay.innerHTML = "rate from " + baseCurrency + " to " + targetCurrency + " is: " + selectedTargetRate;}
+
+
+  // function getRates() {
+  //   var baseCurrency = $("#fromCurrency option:selected").text();
+  //   var targetCurrency = $( "#toCurrency option:selected" ).text();
+  //   var targetCalc = "http://api.fixer.io/latest?symbols=" + targetCurrency;
+  //
+  //   console.log(baseCurrency + " default base (FROM), target is " + targetCurrency );
+  //   var selectedRate = rates[targetCurrency];
+  //   console.log(selectedRate);
+  //   valueDisplay.innerHTML = "rate from " + baseCurrency + " to " + targetCurrency + " is: " + selectedRate;
+  //   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // $.getJSON(targetCalc, function(data){
+  //   var resultObj = data.rates;
+  //   console.log($.parseJSON(data.rates));
+  // });
+
+
+
+// display selected rates onscreen (before clicking calc btn)
+
+// $("#fromCurrency").change(function() {
+//   var selectedChoice = $( "#fromCurrency option:selected" ).text();
+//   selectionFromDisplay.innerHTML = "FROM " + selectedChoice;
+// });
+//
+// $("#toCurrency").change(function() {
+//   var selectedChoice = $( "#toCurrency option:selected" ).text();
+//   selectionToDisplay.innerHTML = "TO " + selectedChoice;
+// });
+
+
+// $("#select").change(function() {
+//   var selectedChoice = $( "#select option:selected" ).text();
+//   selectionDisplay.innerHTML = "selected choice is " + selectedChoice;
+// });
+
+
+
+  // if (baseCurrency = "EUR")???
+
+
+
+    // var valueDisplay = document.getElementById("valueDisplay");
+    // valueDisplay.innerHTML = "Exchange rate from " + baseCurrency + " to " + targetCurrency + " is " + data.rates.USD;
+    // JSON.stringify(data.rates);
+    // valueDisplay.innerHTML = JSON.stringify(JSON.parse(data.rates));
+
+
+//
+// getRates();
+
+
+
+
+
+
+
+
+
+
+// function showSelected(){
+  // $(":select").select(function() {
+  //   selectionDisplay.innerHTML = "something got selected";
+  //
+  // });
+  // var selection = $("#select").val();
+  // selectionDisplay.innerHTML = (selection).toString;
+  // console.log(selection);
+
+  //
+  // var selection = $("#select").val();
+  // console.log($("#select").val());
+// }
+// showSelected();
+
+
+      // JSON.stringify(data.rates);
+      // valueDisplay.innerHTML = JSON.stringify(JSON.parse(data.rates));
+  //   });
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ???[vēlāk ielikt selector funkcijas iekavās un #select aizstāt ar selector, indexā pierakstīt ka izsaucās tas selectors, kad uzspiež ?????]
+
+
+// ---------------------------------
+
+
+
+
+
+
 
 
 
